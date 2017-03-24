fc78df6d66599ea5f7ac8e1620a815a1dfe5c86e
Dmitry Levashov <dio@std42.ru>
2017-03-24 15:30:16 +0300

    code cleanup

--- addon/components/google-mutant-layer.js
+++ addon/components/google-mutant-layer.js
@@ -20,20 +20,10 @@ export default BaseLayer.extend({
 
   trafficLayer : new google.maps.TrafficLayer(),
 
-  _spawned(e) { console.log('_spawned')
-  	this._googleMap = e.mapObject;
-  	// this.get('trafficLayer').setMap(this._googleMap)
-  },
+  isTrafficOn: false,
 
-  // _load() {
-  // 	console.log('load')
-  // 	Ember.run.later(() => { console.log('HERE')
-  // 	  this.get('trafficLayer').setMap(this._googleMap) 
-  // 	}, 1000)
-  // },
-
-  toggleTraffic: Ember.observer('isTrafficOn', function() { console.log(this.get('isTrafficOn'))
-  	this.get('trafficLayer').setMap(this.get('isTrafficOn') ? this._googleMap : null); 
+  toggleTraffic: Ember.observer('isTrafficOn', function() {
+  	this.get('trafficLayer').setMap(this.get('isTrafficOn') ? this._layer._mutant : null); 
   }),
 
   createLayer() {
