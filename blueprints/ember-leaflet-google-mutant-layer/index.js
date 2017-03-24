89ccecd7d6e9ad8a93dc374eab35cb6a6b34803a
Dmitry Levashov <dio@std42.ru>
2017-03-24 14:27:38 +0300

    fix

--- blueprints/ember-leaflet-google-mutant-layer/index.js
+++ blueprints/ember-leaflet-google-mutant-layer/index.js
@@ -3,6 +3,12 @@ module.exports = {
   name: 'ember-leaflet-google-mutant-layer',
   description: 'Ember wrapper for Leaflet.GridLayer.GoogleMutant',
 
+  normalizeEntityName: function() {
+   // this prevents an error when the entityName is
+   // not specified (since that doesn't actually matter
+   // to us
+  },
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject('googlemutant', 'https://gitlab.com/luiscamacho/Leaflet.GridLayer.GoogleMutant.git#e934ab533c3c6797c8f8f17979960fb96dcac36d');
   }
